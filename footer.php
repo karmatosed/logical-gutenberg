94a00efd882ae1b63c28796490ab96fdb27c596e
Tammie Lister <karmatosed@gmail.com>
2017-12-30 01:32:45 +0000

    Remove copyright
    - Will add back in as link block perhaps

--- footer.php
+++ footer.php
@@ -29,17 +29,6 @@
 			</nav><!-- .social-navigation -->
 		<?php endif; ?>
 	</div><!-- .site-info -->
-	<div class="site-copyright">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="https://automattic.com/">Automattic</a>' );
-			?>
-		</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
